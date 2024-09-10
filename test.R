3560c47e1ff15dfb5c2c0201bc8e70ce106de63d
GNAchoux <hi5373934@gmail.com>
2024-09-10 13:48:55 +0900

    R basic study (about operators & variables)

---
+++ test.R
@@ -0,0 +1,8 @@
+version; date(); help("version"); getwd(); 8*3+4; print("Hello R"); 35/4; print("Multiple commands")
+version
+date()
+help("version")
+getwd()
+8*3+4
+35/4
+print("Multiple commands")
